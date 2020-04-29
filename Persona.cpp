6c81db2b86f9fa45cc309a421e12d8020adc7fe5
Luis Ricardo Legreaux Medina <59577641+LuisLegreaux39@users.noreply.github.com>
2020-04-28 22:37:45 -0300

    Mejora de la clase persona
    Falta el metodo de despedida de la clase persona

--- Persona.cpp
+++ Persona.cpp
@@ -6,29 +6,34 @@
 
 using namespace std;
 
+
 class PERSONA{
 public:
-    PERSONA(string _nombre, string _numeroCedula, string _numeroCelular);
-    void presentar();
-    void decir();
-    void despedir();
+    /*Listo*/PERSONA(string _nombre, string _numeroCedula, string _numeroCelular);
+    /*Listo*/void presentar();
+    /*Listo*/void decir(string frase);
+    /*Listo*/void despedir();
     void preguntar();
-    void Mascota();
-    void hacerSaludarMascota(string frase);
+    /*Listo*/string Mascota();
+    /*Listo*/string _getnumeroCedula();
+    /*Listo*/string _getnumeroCelular();
+    /*Listo*/void hacerSaludarMascota(string miNombre);
+    /*Listo*/int iterar();
     string nombre;
     string apellido;
-    string _getnumeroCedula();
-    string _getnumeroCelular();
+    string nombreMascota;
 
 private:
-    void DecirNumeroCedula();
     string numeroCedula;
     string numeroCelular;
+    int iterador;
 };
 PERSONA::PERSONA(string _nombre, string _numeroCedula, string  _numeroCelular){
             this->nombre= _nombre;
             this->numeroCedula = _numeroCedula;
             this->numeroCelular = _numeroCelular;
+            this->nombreMascota = "Murphy";
+            this->iterador = 0;
 }
 string PERSONA::_getnumeroCedula(){
 
@@ -38,19 +43,101 @@ string PERSONA::_getnumeroCelular(){
 
     return numeroCelular;
 }
-void PERSONA::hacerSaludarMascota(string frase){
+int PERSONA::iterar(){
+        return this->iterador + 1;
+}
+
+void PERSONA::decir(string frase = "Hola"){
+
+        string linea;
+
+        for(int i =0 ;i<frase.length() + 2 ;i++){ linea +="*"; }
+
+        printf("  %s \n",linea.c_str());
+
+        printf("  *%s*\n",frase.c_str());
+
+        printf("  %s \n",linea.c_str());
+
+        printf(" ** \n");
+
+        printf(" * \n");
+
+
+}
+void PERSONA::hacerSaludarMascota(string miNombre=""){
+
+        /*Esta funcion debe tener parametro opcionales*/
+
         string __nombre = this->nombre;
-        string frase_completa = ''cowsay \" " +frase + __nombre+'"';
-        char* sistema[100];
-        strcpy(sistema[100],frase_completa);
-        system(sistema[100]);
+
+        int tamanio = miNombre.length();
+
+        this->nombreMascota = ( tamanio > 1 and this->iterador < 2 ) ? miNombre : "Murphy" ;
+
+
+        string frase_completa=" cowsay 'Hola "+__nombre+" mi nombre es "+this->nombreMascota+" ' ";
+
+        system(frase_completa.c_str());
+}
+string PERSONA::Mascota(){
+
+    string __nombreMasccota = this->nombreMascota;
+
+    int centinela = this->iterar();
+
+    this->iterador = centinela;
+
+    string mensaje = (centinela < 2) ? "No nos hemos conocido antes verdad?" : "Hola me llamo "+ this->nombreMascota;
+
+    return mensaje;
+}
+
+void PERSONA::presentar(){
+        string _nombre = this->nombre;
+
+        string fraseCompleta = "Hola me llamo "+ _nombre;
+
+        this->decir(fraseCompleta.c_str());
+
+        printf("  0  \n");
+        printf(" l0l \n");
+        printf("  ll \n");
+
+}
+
+void PERSONA::despedir(){
+
+        string _nombre = this->nombre;
+
+        string fraseCompleta = "Adios ";
+
+        this->decir(fraseCompleta.c_str());
+
+        printf("  0  \n");
+        printf(" l0l \n");
+        printf("  ll \n");
 }
 
 int main(){
+
 PERSONA p("Juan","123456789","8294496461");
+
 cout<<p.nombre<<endl;
+
 cout<<p._getnumeroCedula()<<endl;
+
 cout<<p._getnumeroCelular()<<endl;
-p.hacerSaludarMascota("hola ");
+
+p.hacerSaludarMascota("Pedro");
+
+cout<<p.Mascota()<<endl;
+
+p.presentar();
+
+
+
+
+printf("\n");
 return 0;
 }
